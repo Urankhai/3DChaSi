47b1759a6fea9845ddbfa43cd9664f4dffc4d6b3
Aleksei <urankhai@gmail.com>
2020-01-17 09:01:52 +0100

    Starting to extract only visible walls
    Corrected the number of feasible roads from which walls and buildings are seen.

---
+++ GSCM1/Assets/Visible_Triangles_Extraction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Visible_Triangles_Extraction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject[] Buildings;
+    private GameObject[] Roads;
+    void Start()
+    {
+        Buildings = GameObject.FindGameObjectsWithTag("Reflecting_Obstacles");
+        Roads = GameObject.FindGameObjectsWithTag("Roads");
+
+        Debug.Log("Number of buildings = " + Buildings.Length);
+        Debug.Log("Number of roads = " + Roads.Length);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
